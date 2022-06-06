3c0974e60b9815a766ff50973eeb381a4e8160a5
Sam Higgs <higgs.sam@bcg.com>
2022-06-06 11:43:52 +1000

    feat: Add delete button

--- frontend/startup_generator/lib/components/grocery_item_card.dart
+++ frontend/startup_generator/lib/components/grocery_item_card.dart
@@ -13,26 +13,44 @@ class GroceryItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                Text(
-                  groceryItem.name,
-                  style: ThemeText.bodyText,
-                ),
-                SizedBox(
-                  height: 4,
-                ),
-                Text(groceryItem.categoryLabel, style: ThemeText.caption)
-              ],
+    return Dismissible(
+      key: UniqueKey(), //Key(groceryItem.id.toString()),
+      background: Container(
+        decoration: BoxDecoration(
+          color: Colors.red,
+        ),
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
             ),
-            GroceryItemCheckbox(groceryItem: groceryItem),
-          ],
+          ),
+        ),
+      ),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    groceryItem.name,
+                    style: ThemeText.bodyText,
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(groceryItem.categoryLabel, style: ThemeText.caption)
+                ],
+              ),
+              GroceryItemCheckbox(groceryItem: groceryItem),
+            ],
+          ),
         ),
       ),
     );
